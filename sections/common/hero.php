969c108177bb8060b94cc602bb055a8669c46bd8
Maxspell <m.stizhko@gmail.com>
2025-11-11 14:25:28 +0200

    Add features css

--- sections/common/hero.php
+++ sections/common/hero.php
@@ -6,6 +6,19 @@ if (empty($hero_section) || $hero_section['disabled']) {
 }
 
 $title = $hero_section['title'] ?? '';
+$subtitle = $hero_section['subtitle'] ?? '';
+$image = $hero_section['image'] ?? [];
 ?>
 
-<section class=""></section>
+<section class="page-hero">
+    <div class="page-hero__inner" style="background-image: url();">
+        <div class="page-hero__content">
+            <?php if ($subtitle) : ?>
+                <div class="page-hero__subtitle"><?= esc_html($subtitle); ?></div>
+            <?php endif; ?>
+            <?php if ($title) : ?>
+                <div class="page-hero__title"><?= esc_html($title); ?></div>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
