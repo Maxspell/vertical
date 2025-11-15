f27603c24f2afe13d76237a2e2064e49e2563d62
Maxspell <m.stizhko@gmail.com>
2025-11-15 16:42:02 +0200

    Add fix

--- sections/home/hero.php
+++ sections/home/hero.php
@@ -13,29 +13,41 @@ $image_2 = $hero_section['image_2'] ?? [];
 
 <section class="hero">
     <div class="container container--secondary">
-        <div class="hero__columns">
-            <div class="hero__column">
-                <div class="hero__text">
-                    <?= $text; ?>
-                </div>
-                <div class="hero__image">
-                    <?php if (!empty($image_2)) : ?>
-                        <img src="<?= esc_url($image_2['url']); ?>" alt="<?= esc_attr($image_2['alt']); ?>">
-                    <?php endif; ?>
-                </div>
-                <?php if (!empty($list)) : ?>
-                    <ul class="hero__list">
-                        <?php foreach ($list as $item) : ?>
-                            <li><?= $item['title']; ?></li>
-                        <?php endforeach; ?>
-                    </ul>
-                <?php endif; ?>
+
+        <div class="hero__grid">
+
+            <!-- Левый верх - текст -->
+            <div class="hero__text-block">
+                <div class="hero__text"><?= $text; ?></div>
             </div>
-            <div class="hero__column">
+
+            <!-- Правый большой блок -->
+            <div class="hero__image-main">
                 <?php if (!empty($image_1)) : ?>
                     <img src="<?= esc_url($image_1['url']); ?>" alt="<?= esc_attr($image_1['alt']); ?>">
                 <?php endif; ?>
             </div>
+
+            <!-- Левый нижний сегмент: маленькое фото + список в одной строке -->
+
+            <div class="hero__image-small">
+                <?php if (!empty($image_2)) : ?>
+                    <img src="<?= esc_url($image_2['url']); ?>" alt="<?= esc_attr($image_2['alt']); ?>">
+                <?php endif; ?>
+            </div>
+
+            <?php if (!empty($list)) : ?>
+                <div class="hero__list-block">
+                    <ul class="hero__list">
+                        <?php foreach ($list as $item) : ?>
+                            <li><span></span><?= $item['title']; ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
+
         </div>
+
     </div>
 </section>
